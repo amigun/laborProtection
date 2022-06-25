2845042d8b7148a8b18be3f5b8a292fb4a36000c
Mars <m.babiev05@mail.ru>
2022-06-25 20:12:20 +0500

    developing models

--- labourprotection/entrypoint/models.py
+++ labourprotection/entrypoint/models.py
@@ -39,4 +39,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['full_name', 'password', 'roles', 'kpp', 'inn', 'contacts']
 
     def __str__(self):
-        return self.user_name
+        return self.email
