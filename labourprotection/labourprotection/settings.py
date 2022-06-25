7d2a41fb8f004b172c610bb5218f3b1621591be7
Mars <m.babiev05@mail.ru>
2022-06-25 19:58:01 +0500

    developing models

--- labourprotection/labourprotection/settings.py
+++ labourprotection/labourprotection/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'entrypoint.apps.EntrypointConfig',
+    # 'entrypoint.apps.EntrypointConfig',
+    'entrypoint'
 ]
 
 MIDDLEWARE = [
