727a5d96675ed911457fcd940998538b3707699e
amigun <amigun.tube@gmail.com>
2022-06-26 11:51:39 +0500

    hotfix3.0

--- labourprotection/entrypoint/admin.py
+++ labourprotection/entrypoint/admin.py
@@ -8,18 +8,18 @@ from django.contrib.auth.hashers import check_password
 User = get_user_model()
 
 
-@admin.register(User)
-class UserAdmin(UserAdmin):
-    def authenticate(self, request, username=None, password=None):
-        login_valid = (settings.ADMIN_LOGIN == username)
-        pwd_valid = check_password(password, settings.ADMIN_PASSWORD)
-        if login_valid and pwd_valid:
-            try:
-                user = User.objects.get(username=username)
-            except User.DoesNotExist:
-                user = User(username=username)
-                user.is_staff = True
-                user.is_superuser = True
-                user.save()
-            return user
-        return None
+# @admin.register(User)
+# class UserAdmin(UserAdmin):
+#     def authenticate(self, request, username=None, password=None):
+#         login_valid = (settings.ADMIN_LOGIN == username)
+#         pwd_valid = check_password(password, settings.ADMIN_PASSWORD)
+#         if login_valid and pwd_valid:
+#             try:
+#                 user = User.objects.get(username=username)
+#             except User.DoesNotExist:
+#                 user = User(username=username)
+#                 user.is_staff = True
+#                 user.is_superuser = True
+#                 user.save()
+#             return user
+#         return None
