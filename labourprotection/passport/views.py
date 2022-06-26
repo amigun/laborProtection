0b7be45aa62a2f9509f20783982006850207a480
amigun <amigun.tube@gmail.com>
2022-06-26 19:46:55 +0500

    Make web-passport v1.1

---
+++ labourprotection/passport/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+
+from .models import *
+from .forms import *
+
+# Create your views here.
+class WebPassportPage(CreateView):
+    form_class = WebPassport
+    template_name = 'passport/index.html'
